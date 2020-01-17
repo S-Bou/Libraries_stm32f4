79c02e271df0085a6e836df9962ae1e6fe9ca41c
S-Bou <bouyo30@gmail.com>
2020-01-17 15:30:38 +0100

    1701201530

--- servos.c
+++ servos.c
@@ -1,4 +1,11 @@
 /*
+Steps for config timer:
+	- Select timer and chanel, in this case TIM3 Channel 1.
+	- Select in channel PWM Generation CH1.
+	- Need pulses for 20 ms frecuncy 50 Hz, to do this config prescaler (PSC - 15 bits value) and ARR.
+	  In this case in clock configuration i have in APB1 Timer clocks 72 MHz.
+	  72 MHz / 50 Hz = 1 440 000 -> Preescaler = 1440-1 and ARR = 1000.
+	
 	Init declaring function that start PWM, in this case with timer 3 (chanel 1):
 	
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
---
+++ tcs34725.h
@@ -0,0 +1,11 @@
+
+#ifndef __TCS34725_H
+#define __TCS34725_H
+
+#include "main.h"
+#include <stdint.h>
+
+
+
+#endif /* __TCS34725_H */
+
